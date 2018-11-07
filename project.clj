aec6b8e804550e20a5734f3c9e6845c0c8427292
Nicolas Modrzyk <nico@karabiner-software.com>
2018-11-07 17:44:16 +0900

    first batch

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject cv4.tutorial "0.1.0-SNAPSHOT"
+  :java-source-paths ["java"]
+  :main HelloCv
+  :plugins [[lein-auto "0.1.3"]]
+  :auto {:default {:file-pattern #"\.(java)$"}}
+  :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [origami/origami "4.0.0-beta7"]])
