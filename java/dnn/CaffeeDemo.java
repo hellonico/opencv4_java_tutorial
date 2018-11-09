513674f5fc8ef441e4986aed7575687cc82b6fb1
Nicolas Modrzyk <nico@karabiner-software.com>
2018-11-09 18:02:16 +0900

    caffee working

--- java/dnn/CaffeeDemo.java
+++ java/dnn/CaffeeDemo.java
@@ -14,12 +14,24 @@ public class CaffeeDemo {
     public static void main(String[] args) throws Exception {
         NativeLoader.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        genderDemo();
+        String sourceImageFile = "data/caffee/jeunehomme.jpg";
+        ageDemo(sourceImageFile);
+        genderDemo(sourceImageFile);
 
+        ageDemo("data/caffee/teenager2.jpg");
+        genderDemo("data/caffee/teenager2.jpg");
     }
 
-    private static void genderDemo() {
-        String sourceImageFile = "data/caffee/jeunehomme.jpg";
+    private static void ageDemo(String sourceImageFile) {
+
+        String tfnetFile = "data/caffee/age_net.caffemodel";
+        String protoFil = "data/caffee/age.prototxt";
+        List labels =
+        Arrays.asList("(0, 2)","(4, 6)","(8, 12)","(15, 20)","(25, 32)","(38, 43)","(48, 53)","(60, 100)");
+        runCaffeeNetwork(sourceImageFile, tfnetFile, protoFil, labels);
+    }
+
+    private static void genderDemo(String sourceImageFile) {
         String tfnetFile = "data/caffee/gender_net.caffemodel";
         String protoFil = "data/caffee/gender.prototxt";
         List labels = Arrays.asList("Male", "Female");
@@ -30,18 +42,17 @@ public class CaffeeDemo {
     private static void runCaffeeNetwork(String sourceImageFile, String tfnetFile, String protoFil, List labels) {
         Net net = Dnn.readNetFromCaffe(protoFil, tfnetFile);
         List<String> layernames = net.getLayerNames();
-        System.out.println(layernames);
 
         Mat image = Imgcodecs.imread(sourceImageFile);
-        Mat inputBlob = Dnn.blobFromImage(image, 1.0, new Size(256, 256), new Scalar(0), true, true);
+//        Mat inputBlob = Dnn.blobFromImage(image, 1.0, new Size(256, 256), new Scalar(0), true, true);
+        Mat inputBlob = Dnn.blobFromImage(image, 1.0, new Size(256, 256));
         net.setInput(inputBlob);
         net.setPreferableBackend(Dnn.DNN_BACKEND_OPENCV);
         Mat result = net.forward();
+        result = result.reshape(1,1);
 
         System.out.println(result.dump());
-
         Core.MinMaxLocResult minmax = Core.minMaxLoc(result);
-
         System.out.println(labels.get((int) minmax.maxLoc.x ));
     }
 
