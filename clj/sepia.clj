469c8890d38f2fc1ef9c4b03c2205a975f1ee366
Nicolas Modrzyk <hellonico@gmail.com>
2019-08-19 16:10:04 +0900

    clean up and sepias

--- clj/sepia.clj
+++ clj/sepia.clj
@@ -2,12 +2,12 @@
   (:require
     [opencv4.utils :as u]
     [opencv4.core :refer :all]))
-    
-(-> "data/marcel.jpg"
-  (imread)
-  (transform! 
-    (u/matrix-to-mat [ 
+
+(-> "https://raw.githubusercontent.com/hellonico/origami/master/doc/lena.png"
+  (u/mat-from-url)
+  (transform!
+    (u/matrix-to-mat [
       [0.272 0.534 0.131]
       [0.349 0.686 0.168]
       [0.393 0.769 0.189]]))
-      (u/imshow))
+      (u/imshow {:frame {:width 800 :height 600}}))
--- java/kotlinsample/Sepia.kt
+++ java/kotlinsample/Sepia.kt
@@ -1,34 +1,50 @@
 package kotlinsample
 
-import java.io.IOException
-
-import org.opencv.core.Core
+import java.io.File
+import org.opencv.core.Core.*
 import org.opencv.core.CvType
 import org.opencv.core.Mat
-import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgcodecs.Imgcodecs.*
 import org.scijava.nativelib.NativeLoader
 
 class Sepia {
-
-    @Throws(IOException::class)
-    fun run(input: String, output: String) {
-        // sepia kernel
-        val kernel = Mat(3, 3, CvType.CV_32F)
-        kernel.put(0, 0, 0.272, 0.534, 0.131, 0.349, 0.686, 0.168, 0.393, 0.769, 0.189)
-
-        val destination = Mat()
-        Core.transform(Imgcodecs.imread(input), destination, kernel)
-        Imgcodecs.imwrite(output, destination)
-    }
-
     companion object {
+
+        fun sepiaOne(file: File, outputFolder: File) {
+            val source = imread(file.absolutePath)
+            println("Input file of size ${source.width()}x${source.height()}")
+            val destination = Mat()
+            val kernel = Mat(3, 3, CvType.CV_32F)
+                kernel.put(0, 0, 0.272, 0.534, 0.131, 0.349, 0.686, 0.168, 0.393, 0.769, 0.189)
+            transform(source, destination, kernel)
+            imwrite("${outputFolder.absolutePath}${File.separator}${file.name}", destination)
+        }
+
+        fun sepia(file: File, outputFolder: File) = if (file.isDirectory) {
+            for (f in file.listFiles()) {
+                try {
+                    sepiaOne(f, outputFolder)
+                } catch (e: Exception) {
+                    println("Could not process ${f.absoluteFile}")
+                }
+            }
+        } else {
+            sepiaOne(file, outputFolder)
+        }
 
         @Throws(Exception::class)
         @JvmStatic
         fun main(args: Array<String>) {
-            NativeLoader.loadLibrary(Core.NATIVE_LIBRARY_NAME)
-            Sepia().run("data/marcel.jpg", "target/marcel_sepia.jpg")
-            println("A new sepia cat is in target/marcel_sepia.jpg")
+            if (args.size == 0)
+                println("Usage: Sepia <file1> <file2> ...")
+            else {
+                NativeLoader.loadLibrary(NATIVE_LIBRARY_NAME)
+                val outputFolder = File("sepias")
+                outputFolder.mkdirs()
+                for (file in args) {
+                    sepia(File(file), outputFolder)
+                }
+            }
         }
     }
 }
