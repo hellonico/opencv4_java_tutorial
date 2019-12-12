cbf24d06b60dbeccde8a974ed060b9b61d4779b1
Nicolas Modrzyk <hellonico@gmail.com>
2019-12-12 17:06:23 +0900

    yeah

---
+++ java/me/VggFacesDemo.java
@@ -0,0 +1,41 @@
+package me;
+
+import org.opencv.core.*;
+import org.opencv.dnn.Dnn;
+import org.opencv.dnn.Net;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import origami.ImShow;
+import origami.Origami;
+
+import java.io.File;
+import java.util.List;
+
+import static java.nio.file.Files.readAllLines;
+import static org.opencv.dnn.Dnn.blobFromImage;
+import static org.opencv.imgcodecs.Imgcodecs.imread;
+
+public class VggFacesDemo {
+
+    public static void main(String[] args) throws Exception {
+        Origami.init();
+        List netSpec = origami.Dnn.readNetFromSpec("networks.caffe:vgg-faces:1.0.0");
+        List<String> labels = (List<String>) netSpec.get(2);
+        String path = ((File) netSpec.get(3)).getAbsolutePath();
+        runCaffeeNetwork((Net) netSpec.get(0), path+"/Daniel.jpg", labels);
+    }
+
+    private static void runCaffeeNetwork(Net net, String sourceImageFile, List<String> labels) {
+        Mat image = imread(sourceImageFile);
+        Mat inputBlob = blobFromImage(image, 1.0, new Size(224, 224), new Scalar(129.1863,104.7624,93.5940), false);
+        net.setInput(inputBlob);
+        net.setPreferableBackend(Dnn.DNN_BACKEND_OPENCV);
+        Mat result = net.forward("prob");
+        result = result.reshape(1, 1);
+        Core.MinMaxLocResult minmax = Core.minMaxLoc(result);
+        String name = labels.get((int) minmax.maxLoc.x);
+        Imgproc.putText(image,name, new Point(50,50),Imgproc.FONT_HERSHEY_PLAIN, 3.0, new Scalar(255,255,255),2);
+        ImShow.show(image);
+    }
+
+}
--- java/webcam/SimpleCam.java
+++ java/webcam/SimpleCam.java
@@ -1,26 +1,17 @@
 package webcam;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
-import java.io.File;
-
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.videoio.VideoCapture;
-import org.scijava.nativelib.NativeLoader;
+import me.filters.HueSaturationValue;
+import me.filters.android.Cartoon;
 import origami.Camera;
+import origami.Filter;
+import origami.Origami;
 
-public class OpenCVWebcam {
+public class SimpleCam {
 
     public static void main(String args[]) throws InterruptedException {
-        new Camera().fullscreen().run();
+        Origami.init();
+//        new Camera().filter(new HueSaturationValue.Lomo()).run();
+        new Camera().filter(new Cartoon()).run();
     }
 
 }
